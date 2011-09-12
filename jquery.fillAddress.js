ddfa428d37e3dc309e20eac91ae959a8f6b5a897
Marcos Moura <marcos.moura@grupofolha.com.br>
2011-09-12 17:55:35 -0300

    plugin Jquery to autocomplete address form

---
+++ jquery.fillAddress.js
@@ -0,0 +1,92 @@
+﻿/*
+* @Copyright (c) 2011 Marco Moura (email@marcomoura.com)
+* @Page http://github.com/marcomoura
+* @Version: 0.1
+* @Release: 2011-09-05
+* @author Marco Moura
+* @use Atribuir o método fillAddress() em um input de cep, o autopreenchimento será através do evendo Change(), os parâmetros são opcionais, fornecer apenas ser for necessário alterar os valores dos nomes dos inputs e da url do servidor para consulta do CEP
+* $("#cep").fillAddress({
+*		inputs = {
+*			"address_type" : "id_input_type_address",
+*			"address" : "id_input_address",
+*			"neighborhood" : "id_input_neighborhood",
+*			"city" : "id_input_city",
+*			"uf" : "id_input_uf",
+			"postal_code" : "id_input__postal_code"
+*			},	
+*		 options = {
+*			"server" = : "http://dev.hast.corp.folha.com.br/do.action.wrapper.php"
+*		}
+* });
+* @todo  event click em um botão
+*/
+
+(function( $ ) {
+	$.fn.fillAddress = function( options ) {
+		var st_fillAddress = new plugin_fillAddress( options );
+		this.change( function() { 
+			var postal_code = $( this ).val() ;
+			st_fillAddress.ajax( postal_code ) ;
+		 } ) ;
+	};
+
+	function plugin_fillAddress ( options ){
+
+		this.warning = "warn";
+		this.loading = "load";
+		this.success = "success";
+	
+		this.settings = {
+			"input" : {
+				"address_type" : "address_type" ,
+				"address" : "address" ,
+				"neighborhood" : "neighborhood" ,
+				"city" : "city" ,
+				"state" : "state" ,
+				"postal_code" : "client_postal_code"
+				} ,
+			"server"   : "/anuncie/endereco/busca" ,
+		 } ;
+
+		if ( options ) { 
+			$.extend( settings, options ) ;
+		}
+		
+		this.is_valid = function( postal_code ) {
+			return ( postal_code.match(/^\d+$/) != null ) ? true : false ;
+		} ,
+
+		this.display_error = function( message ) {
+			this.status( message , this.warning ) ;
+			$( "#" + this.settings.input.postal_code ).focus() ;
+			alert( message ) ;
+		} ,
+
+		this.status = function( _message , _css_class ){
+			$( "#filladdress_status span" ).clearQueue();
+			$( "#filladdress_status span" ).attr( "class" , _css_class ).fadeIn( 'slow' ).text( _message ).delay( 5000 ).fadeOut( 'fast' ) ;
+		} ,
+
+		this.ajax = function( postal_code ){
+			var _in = this.settings.input ;
+			if( ! this.is_valid( postal_code ) ) {
+				this.display_error( "O CEP dever ser um valor numérico" ) ;
+				return false;
+			}
+			this.status( "Buscando, aguarde..." , this.loading ) ;
+			var url = this.settings.server + "?postal_code=" + postal_code + "&" + Math.random() + "&timer_off=1" ;
+			$.getJSON(  url, function() { 
+				} ).success( $.proxy( function( JSON ) {
+					var _capitalize_address_type = JSON.address_type[0].toUpperCase() + JSON.address_type.slice(1).toLowerCase() ;
+					$( "#" + _in.address_type ).val( _capitalize_address_type ) ;
+					$( "#" + _in.address ).val( JSON.address ) ;
+					$( "#" + _in.neighborhood ).val( JSON.neighborhood ) ;
+					$( "#" + _in.city ).val( JSON.city ) ;
+					$( "#" + _in.state ).val( JSON.state ) ;
+					this.status( "Endereço localizado com sucesso." , this.success )
+			} , this )).error( $.proxy( function() { 
+				this.display_error( "CEP não localizado" );
+			}, this ) ) ;
+		} ;
+	}
+})( jQuery ) ;
